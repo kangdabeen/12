f4aada92224bb479aae989d140560d262c990ee6
kangdabeen <kdb715@naver.com>
2022-11-17 17:12:51 +0900

    01

---
+++ bingoBoard.c
@@ -0,0 +1,54 @@
+
+#include <stdio.h>
+#include "bingoBoard.h"
+
+#define BINGONUM_HOLE   -1
+
+
+int bingoBoard[N_SIZE][N_SIZE];
+
+
+
+void bingo_init(void)
+{
+	int i, j;
+	int cnt = 1;
+	
+	for (i=0; i<N_SIZE; i++)
+		for (j=0; j<N_SIZE; j++)
+		{
+			if (cnt==15)
+			{
+				bingoBoard[i][j]= BINGONUM_HOLE;
+				cnt++;
+			}
+			bingoBoard[i][j] = cnt++;
+		}
+}
+
+void bingo_print(void)
+{
+	int i, j;
+	
+	printf("========================\n");
+	for (i=0; i<N_SIZE; j++) {
+		for(j=0; j<N_SIZE; j++) {
+			if (bingoBoard[i][j]==BINGONUM_HOLE)
+				printf("X\t");
+			else
+				printf("%i\t", bingoBoard[i][j]);
+		}
+		printf("\n");
+	}
+	printf("=======================\n\n");
+}
+
+void bingo_inputNum(int sel)
+{
+	
+}
+
+int bingo_countCompletedLined(void)
+{
+  
+}
